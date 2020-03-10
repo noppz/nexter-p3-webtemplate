1a73bb1cd5193e0a2516c92cb4a01bcb71a800a7
omg-chaiwat <omg.chaiwat@gmail.com>
2020-03-10 13:56:48 +0700

    add item store

---
+++ app-starter/components/core/AppNotification.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-snackbar
+    id="core-app-notification"
+    v-model="notify"
+    :color="snackbarColor"
+    :timeout="snackbarTimeout"
+    multi-line
+    top
+    right
+    dark
+    style="top: 80px;"
+  >
+    <div>{{ snackbarMessages }}</div>
+    <v-btn icon dark @click.stop="setSnackbar(fInput)">
+      <v-icon>
+        mdi-close-circle
+      </v-icon>
+    </v-btn>
+  </v-snackbar>
+</template>
+
+<script>
+// Utilities
+import { mapActions, mapGetters } from 'vuex'
+
+export default {
+  data: () => ({
+    tInput: true,
+    fInput: false
+  }),
+  computed: {
+    ...mapGetters({
+      snackbar: 'app/getSnackbar',
+      snackbarColor: 'app/getSnackbarColor',
+      snackbarTimeout: 'app/getSnackbarTimeout',
+      snackbarMessages: 'app/getSnackbarMessages'
+    }),
+    notify: {
+      get() {
+        return this.snackbar
+      },
+      set(val) {
+        this.setSnackbar(val)
+      }
+    }
+  },
+  methods: {
+    ...mapActions({
+      setSnackbar: 'app/setSnackbar',
+      setSnackbarColor: 'app/setSnackbarColor',
+      setSnackbarTimeout: 'app/setSnackbarTimeout',
+      setSnackbarMessages: 'app/setSnackbarMessages'
+    })
+  }
+}
+</script>
